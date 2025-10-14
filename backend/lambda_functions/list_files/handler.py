f176366cab63f2784468669c1613be4395fbb766
jkuschner <joshua.kuschner@gmail.com>
2025-10-13 18:58:33 -0700

    implement hello world lambda function

--- backend/lambda_functions/list_files/handler.py
+++ backend/lambda_functions/list_files/handler.py
@@ -3,13 +3,16 @@ import json
 
 def lambda_handler(event, context):
     """
-    A hello world function to confirm basic execution
+    The entry point for the AWS Lambda execution.
+    Returns a basic 200 OK JSON response.
     """
     return {
         'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
         'body': json.dumps({
-            'message': 'Hello from list_files!',
-            'count': 0,
+            'message': 'Hello from list_files Lambda! Environment is active.',
             'files': []
         })
     }
