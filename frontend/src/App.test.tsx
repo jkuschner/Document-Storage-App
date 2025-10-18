4c87545892459a090724ed0132353f6a7208d67a
chrish0873 <chrish4202003@gmail.com>
2025-10-17 19:42:47 -0700

    modify frontend tests to pass test pipeline

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -11,17 +11,13 @@ test('renders learn react link', () => {
 */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders login page', () => {
   render(
-    <MemoryRouter initialEntries={['/login']}>
       <App />
-    </MemoryRouter>
   );
 
-  // This matches the <h2>Login</h2> in Login.tsx
   const loginHeading = screen.getByText(/login/i);
   expect(loginHeading).toBeInTheDocument();
 });
--- frontend/src/__tests__/Login.test.tsx
+++ frontend/src/__tests__/Login.test.tsx
@@ -1,15 +1,24 @@
 
 import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Login from "../components/Auth/Login.tsx";
 import "@testing-library/jest-dom";
 
 test("renders login form", () => {
-  render(<Login />);
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
   expect(screen.getByText(/Login/i)).toBeInTheDocument();
 });
 
 test("handles input changes", () => {
-  render(<Login />);
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
   const emailInput = screen.getByLabelText(/email/i);
   fireEvent.change(emailInput, { target: { value: "test@example.com" } });
   expect((emailInput as HTMLInputElement).value).toBe("test@example.com");
