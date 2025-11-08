37b0072c0ec4be36bdf458ed93678bfc745e7e24
chrish0873 <chrish4202003@gmail.com>
2025-11-08 13:55:30 -0800

    fix handler.py to get deletefile test to pass

--- backend/lambda_functions/delete_file/handler.py
+++ backend/lambda_functions/delete_file/handler.py
@@ -2,13 +2,8 @@ import json
 import os
 import boto3
 
-#s3_client = boto3.client('s3')
-#dynamodb = boto3.resource('dynamodb')
-
-#FILE_BUCKET_NAME = os.environ.get('FILE_BUCKET_NAME', 'file-storage-dev')
-#FILES_TABLE_NAME = os.environ.get('FILES_TABLE_NAME', 'files-dev')
-#files_table = dynamodb.Table(FILES_TABLE_NAME)
-
+FILE_BUCKET_NAME = os.environ.get('FILE_BUCKET_NAME', 'file-storage-dev')
+FILES_TABLE_NAME = os.environ.get('FILES_TABLE_NAME', 'files-dev')
 
 def lambda_handler(event, context):
     """
@@ -16,10 +11,8 @@ def lambda_handler(event, context):
     """
     s3_client = boto3.client('s3')
     dynamodb = boto3.resource('dynamodb')
-    FILE_BUCKET_NAME = os.environ.get('FILE_BUCKET_NAME', 'file-storage-dev')
-    FILES_TABLE_NAME = os.environ.get('FILES_TABLE_NAME', 'files-dev')
     files_table = dynamodb.Table(FILES_TABLE_NAME)
-
+    
     try:
         # Get fileId from path parameters
         path_params = event.get('pathParameters', {})
