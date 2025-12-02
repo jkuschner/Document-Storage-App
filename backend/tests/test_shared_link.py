13e146723a44f43cae1f348173bd6e2b7f045233
chrish0873 <chrish4202003@gmail.com>
2025-12-01 23:08:54 -0800

    fixing backend tests for test-pipeline #16

--- backend/tests/test_shared_link.py
+++ backend/tests/test_shared_link.py
@@ -35,7 +35,8 @@ def dynamodb_table(aws_env):
 def s3_bucket(aws_env):
     s3 = boto3.client('s3', region_name='us-west-2')
     s3.create_bucket(Bucket='test-file-bucket',
-        CreateBucketConfiguration={'LocationConstraint': 'us-west-2')
+        CreateBucketConfiguration={'LocationConstraint': 'us-west-2'}
+    )
     s3.put_object(
         Bucket='test-file-bucket',
         Key='test-user/test-file/document.pdf',
